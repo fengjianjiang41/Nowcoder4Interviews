fb02765090ee2f54093bf08b8d648e02120ae551
Fengjian Jiang <116087936+fengjianjiang41@users.noreply.github.com>
2025-07-19 17:13:44 +0800

    Create AB24 二分查找-I.cpp

---
+++ AB24 二分查找-I.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        //从数组首尾开始，直到二者相遇
+        while(l <= r){ 
+            //每次检查中点的值
+            int m = (l + r) / 2; 
+            if(nums[m] == target)
+                return m;
+            //进入左的区间
+            if(nums[m] > target) 
+                r = m - 1;
+            //进入右区间
+            else 
+                l = m + 1;
+        }
+        //未找到
+        return -1; 
+    }
+};
